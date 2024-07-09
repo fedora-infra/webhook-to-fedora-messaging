d9c43a7ccd0b12770e5d0bcfe52f03123682d31f
Super User <barankara5@gmail.com>
2024-07-10 01:50:02 +0300

    resolve review

--- webhook_to_fedora_messaging/endpoints/apikey.py
+++ webhook_to_fedora_messaging/endpoints/apikey.py
@@ -4,20 +4,26 @@ from ..models.apikey import APIKey
 from ..models.user import User
 from datetime import datetime
 from sqlalchemy_helpers import get_or_create
-from .util import not_found, success, bad_request, created, conflict, validate_request, exclude_from_val
+from .util import not_found, success, bad_request, created, conflict, validate_request, unprocessable_entity
+
 
 app = Flask(__name__)
 apikey_endpoint = Blueprint("apikey_endpoint", __name__)
 
+
 @apikey_endpoint.route("/apikey", methods=["POST"])
 def create_apikey():
+    """Used for creating a new service by sending a post request to /service/ path.
+        
+        Request body:
+            username: Username of the user that api key belongs to
+            valid_till: Time the api key will be valid until
+            name: Name of the api key.
     """
-        Used for creating a new service by sending a post request to /service/ path.
-    """
-    
     session = db.Session()
     body = request.json
-    
+    if not validate_request(request, fields=['username', 'valid_till', 'name',]):
+        return unprocessable_entity()
     
     user = session.query(User).filter(User.username == body['username']).first()
     if user is None:
@@ -34,7 +40,14 @@ def create_apikey():
     
 @apikey_endpoint.route("/apikey/search", methods=["GET"])
 def list_apikey():
-    
+    """Used for listing all api keys by sending a get request to /apikey/search path.
+        
+        Request body:
+            username: Username of the user
+    """
+    if not validate_request(request):
+        return unprocessable_entity()
+        
     session = db.Session()
     user = session.query(User).filter(User.username.like(request.json['username'])).first()
     if user is None:
@@ -46,11 +59,14 @@ def list_apikey():
     
     
 @apikey_endpoint.route("/apikey", methods=["GET"])
-@exclude_from_val
 def lookup_apikey():
-    
+    """Used for searching api keys by sending a get request to /apikey path
+        
+        Request body:
+            uuid: UUID of the apikey
+    """
     if not validate_request(request, fields=['uuid']):
-        return bad_request()
+        return unprocessable_entity
     
     session = db.Session()
     apikey = session.query(APIKey).filter(APIKey.id == request.json['apikey_uuid']).first()
@@ -59,12 +75,20 @@ def lookup_apikey():
         return not_found()
     else:
         valid_till = datetime.strftime(apikey.expiry_date, "%Y-%m-%d")
-        return success({'uuid': apikey.id, 'name': apikey.name, 'valid_till': valid_till, 'valid': not apikey.disabled})
-    
+        return success({'uuid': apikey.id, 'name': apikey.name, 'valid_till': valid_till, 'valid': not apikey.disabled})  
     
 
 @apikey_endpoint.route("/apikey/revoke", methods=["PUT"])
 def revoke_service():
+    """Used for revoking an api key by sending a PUT request to /apikey/revoke path.
+        
+        Request body:
+            username: Username of the user that the api key belongs to.
+            apikey_uuid: UUID of the api key.
+    """
+    if not validate_request(request):
+        return unprocessable_entity()
+    
     session = db.Session()
     user = session.query(User).filter(User.username == request.json['username']).first()
     apikey = session.query(APIKey).filter(APIKey.user_id == user.id and APIKey.id == request.json['apikey_uuid']).first()
@@ -74,12 +98,7 @@ def revoke_service():
         apikey.disabled = True
         session.commit()
         return success({'uuid': apikey.id, 'is_valid': not apikey.disabled})
-    
 
-@apikey_endpoint.before_request
-def validate():
-    if not validate_request(request):
-        return bad_request()
 
 
 
