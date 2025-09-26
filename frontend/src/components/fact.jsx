2c532ed610e80c5c4dd1a92bff8e8c41e4a7e08d
Aurélien Bompard <aurelien@bompard.org>
2025-09-26 16:10:04 +0200

    Loop on the service types wherever possible
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- frontend/src/components/fact.jsx
+++ frontend/src/components/fact.jsx
@@ -1,30 +1,19 @@
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
-import discourse from "./fact/discourse.md?raw";
-import fogo from "./fact/fogo.md?raw";
-import gthb from "./fact/gthb.md?raw";
-import gtlb from "./fact/gtlb.md?raw";
-
-const body = {
-  fogo: fogo,
-  gthb: gthb,
-  gtlb: gtlb,
-  discourse,
-};
-
-const head = {
-  fogo: "Setting up webhook binds from Forgejo repositories",
-  gthb: "Setting up webhook binds from GitHub repositories",
-  gtlb: "Setting up webhook binds from GitLab repositories",
-  discourse: "Setting up webhook binds from Discourse forums",
-};
+import { ServiceTypes } from "../config/data";
 
 function FactDocs({ bind }) {
+  const [mdDoc, setMdDoc] = useState("");
+  useEffect(() => {
+    import(`./fact/${bind}.md?raw`).then((loadedDoc) => setMdDoc(loadedDoc.default));
+  }, [bind]);
+
   return (
     <div className="mdcustom">
-      <h2 className="headelem head">{head[bind]}</h2>
+      <h2 className="headelem head">Setting up webhook binds from {ServiceTypes[bind].title}</h2>
       <div className="small">
-        <ReactMarkdown>{body[bind]}</ReactMarkdown>
+        <ReactMarkdown>{mdDoc}</ReactMarkdown>
       </div>
     </div>
   );
