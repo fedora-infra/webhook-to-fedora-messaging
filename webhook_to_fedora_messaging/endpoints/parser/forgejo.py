eeaaba82022cb5adb918a97fbbaf300c74d2eb96
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-10-09 13:06:18 +0530

    Allow for validating first before extracting
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- webhook_to_fedora_messaging/endpoints/parser/forgejo.py
+++ webhook_to_fedora_messaging/endpoints/parser/forgejo.py
@@ -1,5 +1,6 @@
-from json import dumps, loads
+import json
 
+from fedora_messaging.api import Message
 from webhook_to_fedora_messaging_messages.forgejo import ForgejoMessageV1
 
 from ...fasjson import get_fasjson
@@ -13,13 +14,22 @@ class ForgejoParser(BaseParser):
     def _get_topic(self, headers: HeadersDict, body: Body) -> str:
         return f"forgejo.{headers['x-forgejo-event']}"
 
-    async def get_headers_and_data(self) -> tuple[HeadersDict, bytes]:
-        headers = {k.lower(): v for k, v in self._request.headers.items()}
-        data = await self._request.body()
-        if "action_run" in headers["x-forgejo-event"]:
-            info = loads(data.decode("utf-8"))["run"]["event_payload"]
-            data = dumps(info).encode("utf-8")
-        return headers, data
+    async def parse(self) -> Message:
+        headers, data = await self.get_headers_and_data()
+
+        if self._token:
+            await self.validate(headers, data)
+
+        if "action_run" in headers.get("x-forgejo-event", ""):
+            info = json.loads(data.decode("utf-8"))["run"]["event_payload"]
+            data = info.encode("utf-8")
+
+        body = json.loads(data.decode("utf-8"))
+        topic = self._get_topic(headers, body)
+        agent = await self._get_agent(body)
+        return self.message_class(
+            topic=topic, body={"body": body, "headers": headers, "agent": agent}
+        )
 
     async def _get_agent(self, body: Body) -> str | None:
         return await get_fasjson().get_username("forgejo", body["sender"]["login"])
